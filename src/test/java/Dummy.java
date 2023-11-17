35e5592d7f129bce322b34f40f29e759c12a4016
91991 <91991@LAPTOP-S3INP2MO>
2023-11-17 10:52:16 +0530

    tc004 comitted

--- src/test/java/Dummy.java
+++ src/test/java/Dummy.java
@@ -8,6 +8,12 @@ public class Dummy {
 		
 		//TC002
 		System.out.println("new test case");
+		
+		//TC_003
+		System.out.println("TC_003 created");
+		
+		//TC_004
+		System.out.println("TC_004 created");
 
 	}
 
